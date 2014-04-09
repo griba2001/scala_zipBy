2551c99cd909b0faa72b4e0414b708482864b26d
Gabriel Riba Faura <griba2001@gmail.com>
2014-04-09 15:43:22 +0200

    zipAllBy

--- src/main/scala/ZipBy.scala
+++ src/main/scala/ZipBy.scala
@@ -3,26 +3,43 @@ package com.xarxaire.griba
 object ZipBy {
 
   import scala.collection.GenIterable
-  // import scala.collection.mutable.ListBuffer
 
   /** 
    * zipBy method injection
+   *
+   * zip methods implementation adapted from Scala.collection library GenIterable.zip()
    */
   implicit def toZipBy[A](col: GenIterable[A]) = new {
 
     /** 
-     * implementation adapted from Scala.collection library GenIterable.zip()
      *
      * @param f curried function to zip with
      * @param that 
      * @return an Iterator with the f-zipped values
      */
-    def zipBy[A1 >: A, B, C] (f: A1 => B => C) (that: GenIterable[B]): Iterator[C] = new Iterator[C] {
+    def zipBy[A1 >: A, B, C] (f: A1 => B => C) (that: GenIterable[B]) = new Iterator[C] {
 
         private val these = col.iterator
         private val those = that.iterator
+
         def hasNext = these.hasNext && those.hasNext
-        def next() = f(these.next) (those.next)
+
+        def next() = f (these.next) (those.next)
+      }
+
+    def zipAllBy[A1 >: A, B, C] (f: A1 => B => C) (that: GenIterable[B], thisElem: A1, thatElem: B) = new Iterator[C] {
+
+        private val these = col.iterator
+        private val those = that.iterator
+
+        def hasNext = these.hasNext || those.hasNext
+
+        def next() = {
+             val x = if (these.hasNext) these.next else thisElem
+             val y = if (those.hasNext) those.next else thatElem
+          
+             f (x) (y)
+             } 
       }
   }
 }
--- src/test/scala/TestZipBy.scala
+++ src/test/scala/TestZipBy.scala
@@ -5,13 +5,13 @@ object TestZipBy extends Application {
   import com.xarxaire.griba.ZipBy.toZipBy
   
   val li= (1 to 3).toList
-  val li2 = li
+  val li2 = (1 to 4).toList
 
   def sumInts (x:Int) (y:Int) = x+y
   val sumInts2 : (Int, Int) => Int = _ + _
  
   val res1 = li.zipBy (sumInts _) (li2)
-  val res2 = li.zipBy (sumInts2.curried) (li2)
+  val res2 = li.zipAllBy (sumInts2.curried) (li2, 4, 4)
 
   println( res1.toList)  
   println( res2.toList)  
