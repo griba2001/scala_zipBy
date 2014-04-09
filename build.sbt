284ce1a21bc6bb90ad16b5a29886b13c40c76cc4
Gabriel Riba Faura <griba2001@gmail.com>
2014-04-09 21:57:44 +0200

    scalatest with junit

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "zipby"
 version := "1.0"
 
 scalaVersion := "2.10.3"
+
+libraryDependencies += "junit" % "junit" % "4.11" 
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.3" % "test"
--- src/test/scala/TestZipBy.scala
+++ src/test/scala/TestZipBy.scala
@@ -1,18 +1,31 @@
 package test
 
-object TestZipBy extends Application {
+import org.scalatest.junit.JUnitSuite
+// import org.scalatest.junit.AssertionsForJUnit
+// import org.scalatest.junit.Assert._
+import _root_.org.junit.Test
+import _root_.org.junit.Before
+
+class SimpleSuite extends JUnitSuite {
 
   import com.xarxaire.griba.ZipBy.toZipBy
-  
-  val li= (1 to 3).toList
-  val li2 = (1 to 4).toList
+
+  var li: List[Int] = _   // (1 to 3).toList
+  var li2: List[Int] = _ // (1 to 4).toList
 
   def sumInts (x:Int) (y:Int) = x+y
   val sumInts2 : (Int, Int) => Int = _ + _
- 
-  val res1 = li.zipBy (sumInts _) (li2)
-  val res2 = li.zipAllBy (sumInts2.curried) (li2, 4, 4)
 
-  println( res1.toList)  
-  println( res2.toList)  
+  @Before def initialize() {
+    li= (1 to 3).toList
+    li2 = (1 to 4).toList
+  }
+
+  @Test def verify() { // Uses ScalaTest assertions
+    val it1 = (li.zipBy (sumInts _) (li2)).toList
+    val it2 = (li.zipAllBy (sumInts2.curried) (li2, 4, 4)).toList 
+    assertResult (List(2,4,6)) (it1)
+    assertResult (List(2,4,6,8)) (it2)
+  }
+
 }
